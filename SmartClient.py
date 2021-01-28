3e1371bfe3bfc17098d0d1e3863e27c914653833
Albie Samson <albie0019@uvic.ca>
2021-01-28 02:45:22 -0800

    Added code HTTPS, Added part of Mandatory Output Added GetCookies()

--- SmartClient.py
+++ SmartClient.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 
 from socket import *
+from ssl import *
 import argparse
+import re
+
+def GetCookies(data):
+    #Use re to Get Cookies
+    print("b")
 
 def HTTP10Call(Website):
     # Code to call HTTP 1.0
     s = socket(AF_INET, SOCK_STREAM)    # Starting to build socket connection.
     s.connect((Website, 80)) # Connect
     #request_str = "GET " + Website + "/ HTTP/1.1\r\nConnection: keep-alive\r\n\r\n"
-    request_str = "GET / HTTP/1.0\r\nHost:" + Website + "\r\nConnection: keep-alive\r\n\r\n"
+    request_str = "GET / HTTP/1.1\r\nHost:" + Website + "\r\nConnection: keep-alive\r\n\r\n"
     sending = request_str.encode()
     s.send(sending) # Sending request
     data = s.recv(100000)
@@ -16,14 +22,35 @@ def HTTP10Call(Website):
     s.close()
 
 
-def HTTP11Call():
+def HTTP11Call(Website):
     # Code to call HTTP 1.1
-    print("y")
+    s = socket(AF_INET, SOCK_STREAM)    # Starting to build socket connection.
+    s.connect((Website, 80))            # Bind and Connect
+    #request_str = "GET " + Website + "/ HTTP/1.1\r\nConnection: keep-alive\r\n\r\n"
+    request_str = "GET / HTTP/1.1\r\nHost:" + Website + "\r\nConnection: keep-alive\r\n\r\n"
+    sending = request_str.encode()
+    s.send(sending) # Sending request
+    data = s.recv(10000)
+    print(data.decode())  # Printing the recieved data
+    s.close()
+    #print("y")
+    return True
 
+def HTTPSCall(Website):
+    # Code to call HTTPS (HTTP1.1 over ssl)
 
-def HTTPSCall():
-    # Code to call HTTPS (HTTP over ssl)
-    print("x")
+    s = socket(AF_INET, SOCK_STREAM)    # Starting to build socket connection
+    contextobj = wrap_socket(s)         # The socket is now wrapped with contextobj
+    contextobj.connect((Website, 443))  # Bind and Connect
+
+    request_str = "GET / HTTP/1.1\r\nHost:" + Website + "\r\nConnection: keep-alive\r\n\r\n"
+    sending = request_str.encode()
+    contextobj.send(sending) # Sending request
+    data = contextobj.recv(10000)
+    print(data.decode())  # Printing the recieved data
+    contextobj.close()
+    return True
+    #print("x")
 
 def HTTP2Call():
     # Code to call HTTP2
@@ -37,9 +64,13 @@ def main():
     args = parser.parse_args()   
     Website = args.website     # Website name parsed from the command line
     #print(Website)
-    HTTP10Call(Website)
+    DoesH11Connect = HTTP11Call(Website)
+    DoesHSConnect = HTTPSCall(Website)
 
-
+    #Print the results
+    print("Mandatory Output")
+    print("1. Does the website support HTTP1.1: " + str(DoesH11Connect))
+    print("2. Does the website support HTTPS: " + str(DoesHSConnect))
 
 if __name__ == "__main__":
     main()
