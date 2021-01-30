15aed22a6e6bd1d6326364f0a9ce6c78cfc8244d
Albie Samson <albie0019@uvic.ca>
2021-01-29 22:18:08 -0800

    Added Readme.txt, Final Form of code

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+To run this script, open terminal and type:
+> python3 SmartClient.py www.example.domain
+
+To see help, type:
+> python3 SmartClient.py -h
--- SmartClient.py
+++ SmartClient.py
@@ -3,7 +3,6 @@
 from socket import *
 import ssl
 import argparse
-import re
 
 
 def GetCode(data):
@@ -11,7 +10,7 @@ def GetCode(data):
     lines = data.split("\r\n")
     MessageHeader = lines[0]
     MessageWords = MessageHeader.split(" ")
-    print(MessageWords)
+    #print(MessageWords)    #debug
     for word in MessageWords:
         if word.isdigit():
             return int(word)
@@ -21,150 +20,98 @@ def GetCode(data):
 
 def GetCookies(data):
     #Use re to Get Cookies
-    """
-    cookiepatt = "[^Set\-Cookie](\S.+)" # Pattern string to match cookie
-    cookie_list = []              # List to store cookie
-    lines = data.split("\r\n")
-    #print(lines)
-
-    for line in lines:
-        cookie_match = re.match(cookiepatt, line)
-        
-        if cookie_match:
-            cookies = cookie_match.group()
-            cookie_list.append(cookies)
-    """
     cookie_list = []
-    lines = data.split("\r\n")
+    lines = data.split("\r\n") # Seperate whole data into lines
 
     for line in lines:
-        if "Set-Cookie:" in line:
+        if "Set-Cookie:" in line: # Cookie found, store it in list
             cookie_list.append(line)
 
     return cookie_list
 
 
-def HTTP10Call(Website):
-    # Code to call HTTP 1.0
-    s = socket(AF_INET, SOCK_STREAM)    # Starting to build socket connection.
-    s.connect((Website, 80)) # Connect
-    #request_str = "GET " + Website + "/ HTTP/1.1\r\nConnection: keep-alive\r\n\r\n"
-    request_str = "GET / HTTP/1.0\r\nHost:" + Website + "\r\nConnection: keep-alive\r\n\r\n"
-    sending = request_str.encode()
-    s.send(sending) # Sending request
-    data = s.recv(10000)
-    print(data.decode())  # Printing the recieved data
-    s.close()
-
-
 def HTTP11Call(Website):
     # Code to call HTTP 1.1
-    print("** Starting HTTP 1.1 Connection **")
+    print("\n\n*** Starting HTTP 1.1 Connection ***")
 
     s = socket(AF_INET, SOCK_STREAM)    # Starting to build socket connection.
     s.connect((Website, 80))            # Bind and Connect
     #request_str = "GET " + Website + "/ HTTP/1.1\r\nConnection: keep-alive\r\n\r\n"
     request_str = "GET / HTTP/1.1\r\nHost:" + Website + "\r\nConnection: keep-alive\r\n\r\n"
-    print(request_str + "\n")
+    print("\n" + request_str)
     sending = request_str.encode()
     s.send(sending) # Sending request
-    print("**Request Send**")
+    print("***Request Send***\n")
     data = s.recv(10000)
-    print("** Data recieved **" + data.decode())  # Printing the recieved data
+    print("*** Response recieved ***\n" + data.decode() + "\n\n End of response\n")  # Printing the recieved data
     s.close()
     StatusCode = GetCode(data.decode())     # Get the http status code from data message
 
+    cookie_list = GetCookies(data.decode(errors = 'ignore'))
+
     if(StatusCode in range(300, 399) or StatusCode == 200):
-        return True
+        return (True, cookie_list)
     else:
-        return False
+        return (False, cookie_list)
     
 
 
 def HTTPSCall(Website):
     # Code to call HTTPS (HTTP1.1 over ssl)
-    print("** Starting HTTPS Connection **")
+    print("\n\n*** Starting HTTPS Connection ***")
 
     s = socket(AF_INET, SOCK_STREAM)    # Starting to build socket connection
     contextobj = ssl.wrap_socket(s)         # The socket is now wrapped with contextobj
     contextobj.connect((Website, 443))  # Bind and Connect
 
     request_str = "GET / HTTP/1.1\r\nHost:" + Website + "\r\nConnection: keep-alive\r\n\r\n"
-    print(request_str + "\n")
+    print( "\n" + request_str)
     sending = request_str.encode()
     contextobj.send(sending)            # Sending request
     data = contextobj.recv(10000)
-    print("** Data recieved **" + data.decode())               # Printing the recieved data
+    print("*** Data recieved ***\n" + data.decode())               # Printing the recieved data
     contextobj.close()
 
-    cookie_list = GetCookies(data.decode())
-    for cookie in cookie_list:
-        crumble = cookie.split(";")
-        print("\nCookie-Name:" + crumble[0].split(":")[1])
-        for c in crumble:
-            if "expires" in c:
-                print("Expiry:" + c.split("=")[1])
-            if "domain" in c:
-                print("Domain:" + c.split("=")[1])
+    cookie_list = GetCookies(data.decode(errors = 'ignore'))
 
     StatusCode = GetCode(data.decode())
     if(StatusCode in range(300, 399) or StatusCode == 200):
-        return True
+        return (True, cookie_list)
     else:
-        return False
+        return (False, cookie_list)
 
 
 def HTTP2Call(Website):
     # Code to call HTTP2
-    print("** Starting HTTP2 Connection **")
+    print("\n\n*** Starting HTTP2 Connection ***")
 
-    """
-    s = socket(AF_INET, SOCK_STREAM)    # Starting to build socket connection.
-    s.connect((Website, 80))            # Bind and Connect
-    #request_str = "GET " + Website + "/ HTTP/1.1\r\nConnection: keep-alive\r\n\r\n"
-    request_str = "GET / HTTP/1.1\r\nHost:" + Website + "\r\nConnection: Upgrade, HTTP2-Settings\r\nUpgrade: h2c\r\nConnection: keep-alive\r\n\r\n"
-    print(request_str + "\n")
-    sending = request_str.encode()
-    s.send(sending) # Sending request
-    print("**Request Send**")
-    data = s.recv(10000)
-    print("** Data recieved **" + data.decode())  # Printing the recieved data
-    s.close()
-    StatusCode = GetCode(data.decode())     # Get the http status code from data message
-
-    if(StatusCode in range(300, 399) or StatusCode == 200):
-        return True
-    else:
-        return False
-    """
-    #context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    context = ssl.create_default_context()
-    context.set_alpn_protocols(['h2', 'http/1.1'])
+    def_context = ssl.create_default_context()
+    def_context.set_alpn_protocols(['h2', 'http/1.1'])
     
     s = socket(AF_INET, SOCK_STREAM)    # Starting to build socket connection
-    s.connect((Website, 443))  # Bind and Connect
-    contextsocketobj = context.wrap_socket(s, server_hostname= Website)     # The socket is now wrapped with contextobj
+    s.connect((Website, 443))           # Bind and Connect
+    contextsocketobj = def_context.wrap_socket(s, server_hostname= Website)     # The socket is now wrapped with contextsocketobj
 
     request_str = "GET / HTTP/1.1\r\nHost:" + Website + "\r\nConnection: keep-alive\r\n\r\n"
-    print(request_str + "\n")
+    print(request_str)
     sending = request_str.encode()
     contextsocketobj.send(sending)            # Sending request
-    data = contextsocketobj.recv(10000)
-    #print("** Data recieved **" + data.decode())               # Printing the recieved data
-    print(contextsocketobj.selected_alpn_protocol())
-    result = True
+    #data = contextsocketobj.recv(10000)
+
+    result = False
+    if contextsocketobj.selected_alpn_protocol() == 'h2':
+        print("Selected ALPN Protocol: H2\n")
+        result = True
+    else:
+        print("Selected ALPN Protocol: Not H2\n")
+        result = False
     contextsocketobj.close()
-    '''
-    cookie_list = GetCookies(data.decode())
-    for c in cookie_list:
-        print(c)
-    
-    StatusCode = GetCode(data.decode())
-    if((StatusCode in range(300, 399) or StatusCode == 200) and result):
+
+    if(result):
         return True
     else:
         return False
-    '''
+    
 
 def main():
 
@@ -172,18 +119,33 @@ def main():
     parser.add_argument("website", help="Add the website you wish to contact. Format (www.<website name>.<domain>)") 
     args = parser.parse_args()   
     Website = args.website     # Website name parsed from the command line
-    #print(Website)
-    print("\n** Intermediate Output**\n")
-    #DoesH11Connect = HTTP11Call(Website)
-    DoesHSConnect = HTTPSCall(Website)
-    #DoesHTTP2Connect = HTTP2Call(Website)
+
+    print("\n** Intermediate Output (Optional)**\n")
+    (DoesH11Connect, c_list1) = HTTP11Call(Website)
+    (DoesHSConnect, c_list2) = HTTPSCall(Website)
+    DoesHTTP2Connect = HTTP2Call(Website)
+
+    cookie_list = c_list2 if DoesHSConnect else c_list1
+    Cookiestr = ""
+    for cookie in cookie_list:
+        crumble = cookie.split(";")
+        Cookiestr += "\n\nCookie-Name:" + crumble[0].split(":")[1].split("=")[0]
+        for c in crumble:
+            if "expires" in c:
+                Cookiestr += "\nExpiry:" + c.split("=")[1]
+            if "Expires" in c:
+                Cookiestr += "\nExpiry:" + c.split("=")[1]
+            if "domain" in c:
+                Cookiestr += "\nDomain:" + c.split("=")[1] 
+            if "Domain" in c:
+                Cookiestr += "\nDomain:" + c.split("=")[1] 
 
     #Print the results
     print("**Mandatory Output**")
-    #print("1. Does the website support HTTP1.1: " + str(DoesH11Connect))
-    print("2. Does the website support HTTPS: " + str(DoesHSConnect))
-    #print("3. Does the website support HTTP2: " + str(DoesHTTP2Connect))
-    print("4. Cookies Recieved: ")
+    print("1. Does the website support HTTPS: " + ("Yes" if DoesHSConnect else "No"))
+    print("2. Does the website support HTTP1.1: " + ("Yes" if DoesH11Connect else "No"))
+    print("3. Does the website support HTTP2: " + ("Yes" if DoesHTTP2Connect else "No"))
+    print("4. Cookies Recieved: " + Cookiestr + "\n")
 
 
 if __name__ == "__main__":
