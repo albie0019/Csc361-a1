fc4df1c211b1730617795ec0c8ad1eae5e79d0bf
Albie Samson <albie0019@uvic.ca>
2021-02-21 16:27:36 -0800

    Added README.txt, downloaded basic_structures.py, now adding more classes as needed

--- a2/TrafficAnalysis.py
+++ a2/TrafficAnalysis.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
 
-print("Hello there!")
+import struct
+import argparse
+import sys
+
+def OPEN_FILE():
+    # open the cap file from argumnet
+    parser = argparse.ArgumentParser()                                               
+    parser.add_argument("cap_file", help="Specify the cap file you wish to open") 
+    args = parser.parse_args()   
+    cap_file = args.cap_file   # Website name parsed from the command line
+    #Add checks for error handling before returning to main (if file ends in .cap, if file specified etc.)
+    return cap_file
+
+
+def main():
+    cap_file = OPEN_FILE()
+    print(cap_file)
+
+
+
+if __name__ == "__main__":
+    main()
